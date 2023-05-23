64a137382a31cb27e1c4749b2d93733dbad72c45
Sergey Kim <sergkim7@gmail.com>
2023-05-23 16:21:29 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Database
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT', 5432)
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
+TEST_DB_NAME = os.environ.get('TEST_DB_NAME')
+
+# Cache
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_DB = os.environ.get('REDIS_DB')
+REDIS_EXP = os.environ.get('REDIS_EXP')
